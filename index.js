94b90256f01bfb38482dc59e71a1be2fc2840e47
mjones1818 <michael.jones0532@gmail.com>
2021-01-14 08:47:06 -0600

    commit tree 05ff74484d74e7145a0b006f798ecf4358adfa04

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+const random = Math.floor(Math.random() *1000) +1
+
+const mod = 16 %6
+
+const max = Math.max(5, 10, 15, 20)
